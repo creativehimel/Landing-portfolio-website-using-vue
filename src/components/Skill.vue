d1599b217f28a1bd3762dd529589efe5aa454cc0
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-01-17 16:13:55 +0600

    Skills component added

---
+++ src/components/Skill.vue
@@ -0,0 +1,123 @@
+<script setup></script>
+<template>
+  <section class="relative">
+    <div class="absolute right-0 top-0">
+      <img src="../assets/images/left-dots.svg" alt="" />
+    </div>
+  </section>
+  <section class="max-w-7xl mx-auto md:my-40 px-5">
+    <div class="flex flex-col sm:flex-row justify-between items-center gap-5">
+      <div class="md:w-1/2 space-y2 mb-5">
+        <div class="relative">
+          <svg
+            width="99"
+            height="42"
+            viewBox="0 0 99 42"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M98.75 21C98.75 23.814 97.4068 26.5164 94.933 28.9976C92.4582 31.4799 88.8647 33.7268 84.4042 35.6191C75.4841 39.4034 63.1438 41.75 49.5 41.75C35.8562 41.75 23.5159 39.4034 14.5959 35.6191C10.1354 33.7268 6.54176 31.4799 4.067 28.9976C1.59324 26.5164 0.25 23.814 0.25 21C0.25 18.186 1.59324 15.4836 4.067 13.0024C6.54176 10.5201 10.1354 8.27324 14.5959 6.3809C23.5159 2.59664 35.8562 0.25 49.5 0.25C63.1438 0.25 75.4841 2.59664 84.4042 6.3809C88.8647 8.27324 92.4582 10.5201 94.933 13.0024C97.4068 15.4836 98.75 18.186 98.75 21Z"
+              stroke="#0DB760"
+              stroke-width="0.5"
+            />
+          </svg>
+          <h5 class="text-primary pl-5 absolute top-2">My Skills</h5>
+        </div>
+        <h1 class="text-5xl font-bold leading-[60px] pr-5 mt-5">
+          Why Hire Me For Your Next <span class="text-primary">Project?</span>
+        </h1>
+        <p class="py-5 md:w-2/3">
+          I’m specialist in UI/UX Designe. My passion is designing & solving problems through user
+          experience and research.
+        </p>
+        <button class="btn bg-primary text-white hover:bg-green-600 py-4 px-">Hire Me</button>
+      </div>
+      <div class="md:w-1/2">
+        <div class="grid grid-rows-3 grid-flow-col gap-4 items-center">
+          <div class="col-span-2 space-y-2 md:w-[255px]">
+            <svg
+              width="28"
+              height="28"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.625 9.625H25.375M1.93703 10.2927L13.2984 25.0288C13.3809 25.1365 13.4872 25.2237 13.6088 25.2837C13.7305 25.3437 13.8643 25.3749 14 25.3749C14.1357 25.3749 14.2695 25.3437 14.3912 25.2837C14.5128 25.2237 14.6191 25.1365 14.7016 25.0288L26.063 10.2927C26.1747 10.1473 26.2395 9.97129 26.2488 9.78814C26.258 9.60498 26.2114 9.42333 26.1149 9.26734L22.2611 3.04664C22.1818 2.91791 22.0708 2.81159 21.9388 2.73779C21.8068 2.664 21.6582 2.62517 21.507 2.625H6.49305C6.34183 2.62517 6.19316 2.664 6.06117 2.73779C5.92918 2.81159 5.81824 2.91791 5.73891 3.04664L1.88508 9.26734C1.78864 9.42333 1.74196 9.60498 1.75124 9.78814C1.76052 9.97129 1.82532 10.1473 1.93703 10.2927V10.2927Z"
+                stroke="#0DB760"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M21.875 3.5L19.25 9.625L14 2.625M6.125 3.5L8.75 9.625L14 2.625M14 24.5L8.75 9.625M14 24.5L19.25 9.625"
+                stroke="#0DB760"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <h4 class="text-xl font-semibold">Visual Design</h4>
+            <p>Create user interface design with unique & modern ideas</p>
+          </div>
+          <div class="row-span-2 col-span-2 space-y-2 md:w-[255px]">
+            <svg
+              width="28"
+              height="28"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.75 10.5L14 3.5L26.25 10.5L14 17.5L1.75 10.5Z"
+                stroke="#0DB760"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M6.125 13.125V20.125L14 24.5L21.875 20.125V13.125M26.25 20.125V10.5M14 17.5V24.5"
+                stroke="#0DB760"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <h4 class="text-xl font-semibold">UX Research</h4>
+            <p>Create digital user products with updated ideas</p>
+          </div>
+          <div class="row-span-2 space-y-2 md:w-[255px]">
+            <svg
+              width="28"
+              height="28"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.625 17.5H6.125L9.625 3.5L13.125 24.5L16.625 12.25L18.375 17.5H21.875"
+                stroke="#0DB760"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M23.625 19.25C24.5915 19.25 25.375 18.4665 25.375 17.5C25.375 16.5335 24.5915 15.75 23.625 15.75C22.6585 15.75 21.875 16.5335 21.875 17.5C21.875 18.4665 22.6585 19.25 23.625 19.25Z"
+                stroke="#0DB760"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+
+            <h4 class="text-xl font-semibold">Design Prototype</h4>
+            <p>Create advance design prototype with Figma apps.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Hero from '@/components/Hero.vue'
 import Feature from '@/components/FeatureSection.vue'
+import Skill from '@/components/Skill.vue'
 </script>
 
 <template>
   <main>
     <Hero />
     <Feature />
+    <Skill />
   </main>
 </template>
