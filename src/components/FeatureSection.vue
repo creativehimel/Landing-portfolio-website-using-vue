84c279f8168682bdfd9b034d5e79e16375151dc7
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-01-17 15:11:11 +0600

    Feature component added

---
+++ src/components/FeatureSection.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+<template>
+  <section class="mt-40 mb-32 max-w-7xl mx-auto">
+    <div class="pt-44 sm:pt-52 md:pt-0 px-5">
+      <div class="flex sm:flex-row justify-around sm:items-start gap-3 mt-32">
+        <div class="space-y-4">
+          <p class="text-4xl text-primary font-[800]">80+</p>
+          <p class="text-xl text-primary font-medium">Satisfied clients</p>
+        </div>
+        <div class="bg-primary w-[2px] mx-auto md:h-20"></div>
+        <div class="space-y-4">
+          <p class="text-3xl text-primary font-[700]">200+</p>
+          <p class="text-xl text-primary font-medium">Projects completed</p>
+        </div>
+        <div class="bg-primary w-[2px] mx-auto md:h-20"></div>
+        <div class="space-y-4">
+          <p class="text-3xl text-primary font-semibold">99+</p>
+          <p class="text-xl text-primary font-medium">Reviews given</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Hero from '@/components/Hero.vue'
+import Feature from '@/components/FeatureSection.vue'
 </script>
 
 <template>
   <main>
     <Hero />
+    <Feature />
   </main>
 </template>
